eabc3d053bd3bc6d614aca8d261063513d692372
hanjaegyeong <hjg000223@naver.com>
2022-12-26 07:16:26 +0900

    feat: 17182_dfs+백트래킹+플로이드워셜(골드3) 주석수정

--- baekjoon/17182_dfs+백트래킹+플로이드워셜(골드3).py
+++ baekjoon/17182_dfs+백트래킹+플로이드워셜(골드3).py
@@ -1,16 +1,16 @@
 #17182_dfs+백트래킹+플로이드워셜(골드3)
 
 #dfs로 백트래킹. 최단비용으로 모든 행성 탐사 min값 ans 갱신***
-def dfs(x, cnt, cost): #현재위치, cnt, cost갱신
+def dfs(now, cnt, cost): #현재위치, cnt, cost갱신
     global result
     if cnt == n:
         result = min(result, cost) #끝까지 도달한 경우들 min값 갱신***
         return
-    for i in range(n):
-        if not visited[i]:
-            visited[i] = 1
-            dfs(i, cnt+1, cost+graph[x][i])
-            visited[i] = 0 #백트래킹
+    for nxt in range(n):
+        if not visited[nxt]:
+            visited[nxt] = 1
+            dfs(nxt, cnt+1, cost+graph[now][nxt]) #현재위치에서 next위치로 가는 비용 갱신
+            visited[nxt] = 0 #백트래킹
 
 n, k = map(int, input().split()) #행성개수, 위치
 graph = list(list(map(int,input().split())) for _ in range(n))
