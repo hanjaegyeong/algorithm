7ebe16da03c911f026394942e871706de0f4ddc9
hanjaegyeong <hjg000223@naver.com>
2022-12-28 06:59:01 +0900

    feat: 플로이드워셜 for문ijk순서 주석

--- baekjoon/10159_플로이드워셜(골드3).py
+++ baekjoon/10159_플로이드워셜(골드3).py
@@ -1,4 +1,4 @@
-#10159_플로이드워셜(골드3)
+#10159_플로이드워셜
 #비교 불가능한 물건 개수
 #플로이드워셜 True/False(거리가중치값 안주어진 문제이므로)
 #a->b, b->a 불가능하면 cnt+=1
@@ -14,12 +14,13 @@ for _ in range(m):
     graph[a-1][b-1] = 1 #1로 표시! true/false
 
 #플로이드워셜 i->j & j->k = i->k
-#여기 순서 맞아야 함!!***i가 k보다 안쪽! i->k이므로
+#가장 바깥쪽 for문은 경유할 정점,가운데 for문은 출발 정점,가장 안쪽 for문은 도착 정점***
 for k in range(n):
     for i in range(n):
         for j in range(n):
             if graph[i][j] and graph[j][k]:
                 graph[i][k] = 1
+                print(i, k)
 
 for i in range(n):
     cnt = 0
