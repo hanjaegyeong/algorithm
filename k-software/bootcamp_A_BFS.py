9dec7c2ab133d74022acce539fd0195b30826f89
hanjaegyeong <hjg000223@naver.com>
2023-02-23 20:05:41 +0900

    feat: 코드 간결화

--- k-software/bootcamp_A_BFS.py
+++ k-software/bootcamp_A_BFS.py
@@ -20,13 +20,16 @@ for _ in range(t):
     n, m = map(int, input().split()) #노드 수, 간선 수
     ans = 0
     graph = list([] for _ in range(n+1))
+    
     for _ in range(m):
         x, y = map(int, input().split())
         graph[x].append(y)
         graph[y].append(x)
+    
     for i in range(1, n+1):
         visited = [0] * (n+1)
         visited[i] = 1
         bfs(i)
         ans = max(ans, max(visited))
+        
     print(ans)
--- k-software/bootcamp_A_플로이드워셜.py
+++ k-software/bootcamp_A_플로이드워셜.py
@@ -5,15 +5,16 @@ input = sys.stdin.readline
 
 t = int(input())
 for _ in range(t):
-    ans = 0
     n, m = map(int, input().split()) # n: 기업 수, m: 관계 수
-    graph = list([0] * (n+1) for _ in range(n+1))
+    ans = 0
+    graph = list([0] * (n+1) for _ in range(n+1)) # graph[i][j] == 1: i번 기업은 j번 기업과 연결되어 있음
     
     for _ in range(m):
         a, b = map(int, input().split()) # 관계 양방향
         graph[a][b] = 1
         graph[b][a] = 1
 
+    # i->j && j->k == i->k
     # j 경유할 때, i 출발 k 도착
     for j in range(n+1):
         for i in range(n+1):
