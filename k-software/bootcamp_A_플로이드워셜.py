260c5655dc477c9b98bfa762e192f89f5b5c0c9a
hanjaegyeong <hjg000223@naver.com>
2023-02-23 18:13:11 +0900

    feat: 주석 추가

--- k-software/bootcamp_A_플로이드워셜.py
+++ k-software/bootcamp_A_플로이드워셜.py
@@ -1,22 +1,27 @@
-#가장 큰 컨소시엄_플로이드워셜
-#a->b && b->c == a->c
+# 가장 큰 컨소시엄_플로이드워셜
+# a->b && b->c == a->c
 import sys
 input = sys.stdin.readline
 
 t = int(input())
 for _ in range(t):
     ans = 0
-    n, m = map(int, input().split()) #n: 기업 수, m: 관계 수
+    n, m = map(int, input().split()) # n: 기업 수, m: 관계 수
     graph = list([0] * (n+1) for _ in range(n+1))
+    
     for _ in range(m):
-        a, b = map(int, input().split()) #관계 양방향
+        a, b = map(int, input().split()) # 관계 양방향
         graph[a][b] = 1
         graph[b][a] = 1
+
+    # j 경유할 때, i 출발 k 도착
     for j in range(n+1):
         for i in range(n+1):
             for k in range(n+1):
                 if graph[i][j] == 1 and graph[j][k] == 1:
                     graph[i][k] = 1
+    
     for i in range(n+1):
         ans = max(graph[i].count(1), ans)
+    
     print(ans)
